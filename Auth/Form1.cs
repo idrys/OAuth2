3deaa9dbc2cd6a6829e1fae12a4acb8bfe789edc
idrys <idrys2@gmail.com>
2017-12-05 18:52:34 +0100

    Update Form1.cs

--- Auth/Form1.cs
+++ Auth/Form1.cs
@@ -29,14 +29,14 @@ namespace Auth
 		{
             if ( dev)
             {
-                token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImp0aSI6ImRmMTNhZTU2NzIwM2E1NzAzMWI2NWZhNzU2ZmU0NmEyMzcxYWFiNzY4YjhhMWFjZWJhODIzZGY2ZGJmYjZiOTk5NGEwZTYwYmI3YWU3MzQzIn0.eyJhdWQiOiIzIiwianRpIjoiZGYxM2FlNTY3MjAzYTU3MDMxYjY1ZmE3NTZmZTQ2YTIzNzFhYWI3NjhiOGExYWNlYmE4MjNkZjZkYmZiNmI5OTk0YTBlNjBiYjdhZTczNDMiLCJpYXQiOjE1MTIyMTMyNDUsIm5iZiI6MTUxMjIxMzI0NSwiZXhwIjoxNTQzNzQ5MjQ1LCJzdWIiOiIxIiwic2NvcGVzIjpbXX0.eIXpzXRluPyg0S7GjJbh6s4p7XzET-RgzdxIYdOMGewg2uVN3Co5Cq4Ei19yMiCOko5qLaOtV1wzklHUckaY3rJ35iLnqrnnaywV7RgfWdzp3UCZ62Oam9wb0hv0Ib2j0laaLw_PpLTX5l3x1IsYLLo9b-XxbssxDwz2aZ6OKHx92e-A49uYpuzvJb6Lu_ZAFVJSDvdtSpI193ey_kOEl3AwnXhTJVUVcfZqE8GkQXT2GrZ9jMRwUIb-1_WI-kRvN1fiZZJgYiO5ZXRDkrJ7hMNifNaPCeaFhX-BXNdwUAxR8z5EtffVNTHss0GjQP6BiKGTRSdlIuNBIo_Tt2qSTUYtTL540U-UsNMJZDZiaSPGA_vMuqEHnJCFSr9CB-Ee1kxYfBNZS7BM_uggFnQY_9S1SnTCYr3siTOkq5RthqZ4U-OCyccnFfEEA_c_gKgOFffCafulE3O5_q6Pk1xLhX3NnJWxmCDtpsyXcfqZAixHHrW9b-pEQy3uchyAxIGQN6cfJL80O3TlWAL74Fug51dDQDoa0y268ddQS-ZunNaACoqQDvTP7xhJkFvY4sQaXtFUHHdQCfvAMYP0q8V7CGNe6bbh2U9VPChJOubY1jCHFBKClYyLKoc4zOlOCc5ziZ2_XkCGq0fjGpSXqV55spWw12jyfYfTo8hatjhZNLQ";
+                token = "YfTo8hatjhZNLQ";
                 page = "https://passport.dev/";
                 
             }
             else
             {
-                token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImp0aSI6IjNmYzIyNzBlODViOTU5NjhiNjJlZWI1NDJjNTliMTZjNDQ4MjYyMmM4NzllMTRjOGIzN2RjOGRmMWUwZTMwZGI3NmMyMWQwNDVlMTllYTJhIn0.eyJhdWQiOiIzIiwianRpIjoiM2ZjMjI3MGU4NWI5NTk2OGI2MmVlYjU0MmM1OWIxNmM0NDgyNjIyYzg3OWUxNGM4YjM3ZGM4ZGYxZTBlMzBkYjc2YzIxZDA0NWUxOWVhMmEiLCJpYXQiOjE1MTIzOTkzODEsIm5iZiI6MTUxMjM5OTM4MSwiZXhwIjoxNTQzOTM1MzgxLCJzdWIiOiIxIiwic2NvcGVzIjpbXX0.pWTvSqcicdlEpuw2m-mQTLlRctvRh0u9hQ4GBGDKtf9NY0Mz6ad0Y31VIcCwUp_9-Ji8OOLA5r08C9tPo5Q17LTvLG6ctyXkucgjTVDOnam2-WxYazDkXWFKZ9Nq4e13H8KJ6ltRzYlLvzS2fXT9VxeYW5KAObEMYeIRhAS_LxjFc74curjzp82hJQ0k7sG0GPl-ex37gp_r-W9N8ZbjuJ4yixbg0iDlI1SZBaPca51edLj3OZUqlV6L69hBqJsVpJB97cwQCIutcFb_y0l6upfDIlNHwwKOeSlzfTEr0i7q1f6uWYB4_9g7tjJWlXTQnGilTJEThxkGWevWXgO2ZoDwnko4Aa370HwZAIj7VxW_YI3lwj6XlQuNqIfA3Z6gnDd-H8JNRL0Qc3elT28SOPuzU_RUE4JeT49sE2kzbtgDNl3SP9-Z5mNHYBve2gIDyTFLKhtc5R6zgVeGUisEt3UVrfSWywG2i7nJcd8Hhory9GtjbkMnLUnbsElY2hZUyDtxt9CjVC7CuQbrOPv-GIbXg2P9dGLkUEGkODMTYtJvP6MJvEnlh8Qx-AIny-FfTXvMb7PWoZYLybgJ94mYV5izSjG7feo4ZwgdPfFv42LKHqjkRYUI3KJuhjDiZHIJD3gFu2wfP8jg-pQeyoyOKKssPokffSGs-0RkWR5Pdnc";
-                page = "https://1sw.pl/";
+                token = "0RkWR5Pdnc";
+                page = "https://passport.pl/";
                
             }
 
